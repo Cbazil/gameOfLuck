a405b22fd0f78c9fa6387b30f796a57388a4a897
Cbazil <chadbosch08@gmail.com>
2020-05-24 22:25:01 +0200

    working onclick holes

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var set = [1, 2, 3 ,4];
+/*
+function(attribute) {
+  
+},var set = [1, 2, 3 ,4];
 
 console.log("Ys->", Math.random());
 console.log(Math.floor(Math.random() * set.length + 1));
@@ -8,3 +11,9 @@ var randomNumber = (min, max) =>{
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 console.log(randomNumber(5, 15));
+*/
+
+
+getNumber = () =>{
+  console.log("Clicked!");
+}
