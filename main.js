5b1df4d994a9839daa6a9af328d37a7d9a63fce3
Cbazil <chadbosch08@gmail.com>
2020-05-24 03:25:01 +0200

    Random numbers Math Arithmetic

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+var set = [1, 2, 3 ,4];
+
+console.log("Ys->", Math.random());
+console.log(Math.floor(Math.random() * set.length + 1));
+
+// random number between
+var randomNumber = (min, max) =>{
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+console.log(randomNumber(5, 15));
